afe065ab79998825d144ed8f3eefdb9f121ef6cb
Joshua Fraser <joshua.fraser@my.jcu.edu.au>
2015-05-15 14:11:39 +1000

    no message

--- PassSystem/client/controllers/controller.js
+++ PassSystem/client/controllers/controller.js
@@ -1,5 +1,31 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    angular.module('my-app-module',
-  ['ngRoute' /* etc */, 'lbServices', 'my-app.controllers']);
+angular.module('myApp',
+  ['lbServices']).controller('Controller', ['$scope', 'Student', 'Staff', 'Subject', function($scope, Student, Staff, Subject) {
+ 	$scope.allStudents = [];
+ 	$scope.allSubjects = [];
+ 	
+ 	//$scope.deleteID = Student.deleteById().$promise.then(function(id){
+ 	//	console.log(id);
+ 	//});
+  	$scope.deleteID = function (itemid){
+  		console.log(itemid);
+  		
+  		Student.deleteById({id: itemid});
+  	}
+
+  	
+
+
+ 	Student.find().$promise.then(function(results){
+ 		$scope.allStudents = results;
+ 	});
+
+
+
+
+ 	Subject.find().$promise.then(function(results){
+ 		$scope.allSubjects = results;
+ 		//console.log(results);
+ 	});
+
+
 }]);
