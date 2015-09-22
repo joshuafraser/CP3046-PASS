a047a248360c4a9fa771795716afd776e47504a7
justinosbaldiston <jmcsquizzy@gmail.com>
2015-09-22 12:58:27 +1000

    Adding CSV parsing into the current system and removing the temporary files. Also added a template.html file

--- PassSystem/client/controllers/controller.js
+++ PassSystem/client/controllers/controller.js
@@ -21,7 +21,7 @@ angular.module('myApp',
 
     $scope.printItem = function(){
       console.log($scope.subjectSelect.code + $scope.weekSelect.name);
-    }
+    };
 
     //Used to modify a table for marking attendance.
     $scope.findSubjectForMarking = function(){
@@ -397,23 +397,75 @@ angular.module('myApp',
    		//console.log(itemid);
    		User.deleteById({id: itemid});
    		geUsers();
-   	}
+   	};
    	$scope.addUser = function (){
    		//console.log($scope.spot);
    		User.create($scope.spot);
          getUsers();
-   	}
+   	};
    	$scope.editUser = function(itemid){
    		//console.log(itemid);
    		$scope.spot = User.findById({id: itemid});
-   	}
+   	};
    	$scope.updateUser = function(){
    		//console.log($scope.spot.studentId);
    		User.upsert($scope.spot);
    		getUsers();
-   	}
+   	};
    	$scope.deselectUser = function(){
    		$scope.spot = "";
-   	}
+   	};
 
+}]).controller("csvController", ['$scope', function($scope) {
+    $scope.MyFiles = [];
+    $scope.data = [];
+
+    $scope.showCSV = function() {
+      if($scope.data.length > 0) {
+        var message = "Do you want to import: \n";
+        for (var i = 0; i < $scope.data.length; i++) {
+          message += " - " + $scope.data[i] + "\n";
+        }
+        message += "to the database?";
+        var confirm = window.confirm(message);
+        if (confirm) {
+          window.alert("added to database");
+        }
+      } else {
+        window.alert("No data present");
+      }
+    };
+
+    $scope.handler = function(event, files) {
+      if(document.getElementById("fileInput").value.split(".")[1] == "csv") {
+        $scope.data = [];
+        var reader = new FileReader();
+        reader.onload = function (event) {
+          var contents = reader.result;
+          contents = contents.trim();
+          var contentsArray = contents.split("\n");
+          for (var i = 1; i < contentsArray.length; i++) {
+            $scope.$apply($scope.data.push(contentsArray[i].trim()));
+          }
+          console.log($scope.data);
+        };
+        reader.readAsText(files[0]);
+      } else {
+        window.alert("That's not a csv!");
+      }
+    };
+  }]).directive("fileChange", ['$parse', function($parse) {
+  return {
+    require: "ngModel",
+    restrict: "A",
+    link: function($scope, element, attrs, ngModel) {
+      var attrHandler = $parse(attrs['fileChange']);
+      var handler = function(e) {
+        $scope.$apply(function() {
+          attrHandler($scope, {$event: e, files: e.target.files})
+        });
+      };
+      element[0].addEventListener('change', handler, false);
+    }
+  }
 }]);
